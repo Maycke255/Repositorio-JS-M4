1c22a1ab278f8a82fa76ec4a6adc7d8de3b215de
Maycke <reydsonk@gmail.com>
2025-07-07 18:19:35 -0300

    Enviando arquivos sobre operador de coalescencia nula

--- Parameters/JS/param.js
+++ Parameters/JS/param.js
@@ -1,7 +1,11 @@
 /* Uma rest param e nada mais nada menos que o resto dos porametros de uma função, ela possibilita declararmos quantos parametros/argumentos quisermos,
 por exemplo, declaramos um unico parametro para a função, em seguida podemos passar quantos valores quisermos nesse oarametro, ela e como se fosse uma
 array, podemos ate ultilizar metodos de array nesse metodo, mas caso quisermos declarar um argumento a mais na função que vamos usar de maneira especifica
-na função, devemos passar antes do rest param */
+na função, devemos passar antes do rest param.
+Isso e util em situações como:
+Manipulação de arrays sem mutar os originais.
+Aceitar inputs dinâmicos em funções utilitárias.
+passar arrays como argumentos de uma função, afinal os parametros são ilimitados. */
 const integers = [3, 7, 13, 5]
 
 const games = ['Super Mario Bross', 'MK11', 'Call of Duty Black Ops 3', 'Pokemon Legends Arceus']
@@ -23,3 +27,45 @@ function numbers (...element) {
 }
 
 console.log(numbers(3, 3))
+
+//=========================================================
+// OPERADOR DE ENCADEAMENTO OPCIONAL
+//=========================================================
+
+/* Geralmente quando tentamos exibir ou acessar uma propriedade que não existe, ela da um erro no console (TypeError: Cannot read properties of undefined
+(reading 'ddd')), ou seja, o erro diz que ele tentou acessar uma propriedade indefinida (inexistente), mas para não dar nenhum erro e não parar o codigo
+usamos o operador de encadeamento opcional "?." e seguido de uma interrogação e o ponto "." que usamos normalmente para encadear alguma coisa a outra, 
+ele faz um tipo de questionamento antes de prosseguir para a proxima ação, assim podemos impedir erros e melhor fluidez no codigo. */
+const user = {
+    name: "John Doe",
+    email: "doejohn@email.com",
+    friends: [{
+        name: "Mary",
+        address: {
+            street: "Some Street",
+            number: 89
+        }
+    }],
+    age: 42,
+    phone: {
+        countryCode: "+55",
+        ddd: "22",
+        number: "998765432"
+    }
+}
+
+// A propriedade phone e ddd não existe, então retorna apenas undefined e não da erro
+const acess = user?.friends[0]?.phone?.ddd
+
+// Uma boa coisa e verificar se a opção não existe e exibir um alerta para o usuario
+if (acess === undefined) {
+    console.log('O conteudo não existe.')
+}
+// ERRO PADRÃO DE ENCADEAMENTO
+// console.log(user?.friends[0].phone.ddd)
+
+//=========================================================
+// OPERADOR DE COALESCÊNCIA NULA (??)
+//=========================================================
+
+/* Esse e um operador meio esquisito, e usado como operador lógico, assim como o && e || para fazer verificações */
