34d43d1e0eafd27c6452be80c9eb5872bab7124e
Maycke <reydsonk@gmail.com>
2025-07-17 16:26:05 -0300

    Aula sobre modularização finalizada.

--- Modules/ES-Modules/js/ESM.js
+++ Modules/ES-Modules/js/ESM.js
@@ -14,3 +14,26 @@ export function somar(a, b) {
 export const name = 'Maycke'
 export const arr = ['Elden ring', 'Dark Souls 3']
 export const { games } = arr
+
+//Exportação em grupo:
+/* A exportação em grupo e algo mais para se organizar o código, ela exporta todos os códigos de uma vez, nela usamos chaves para referenciar o que
+queremos exportar, exemplo:
+export { name, arr, somar } */
+
+/* No caso do export defalut e diferente, não podemos salvar nada em variaveis diretamente, não podemos fazer export default const name = 'Hello World!'
+isso da erro, precisamos exportar diretamente o que queremos, a não ser uma function, ai sim usamos export default function name () {}. Em um arquivo de
+exportação so pode haver SOMENTE UM EXPORT DEFAULT, afinal default quer dizer padrão, não faz sentido ter mais de um padrão, então se declararmos outra
+export default e a mesma coisa de estarmos duplicando o export default que já existe criado. */
+
+//Essa exportação e a forma mais comum, e a forma de exportação inline
+export default function Ola () {
+  return 'Hello World!'
+}
+
+//Mas também podemos usar ela não-inline, podemos apenas declarar elas em qualquer lugar doo codigo usando apenas export {...} <- dessa forma
+//Export default não-inline, exportamos assim, muito mais simples
+// export default Ola
+
+const name2 = 'Moises'
+const name3 = 'Wellington'
+export {name2, name3}
--- Modules/ES-Modules/js/testES.js
+++ Modules/ES-Modules/js/testES.js
@@ -1,5 +1,22 @@
+/* Esse e o arquivo de expotação, onde vamos estar usando os codigos que exportamos no outro araquivo, ele e mais pratico que o common JS, usamos 
+apenas chaves para referenciar os nomes dos modulos que estamos importando, precisam ser os mesmos nomes, para que o arquivo encontre exatamnte
+quais são as importações */
+
+// Usamos a palavra from para referenciar o arquivo de exportação, da onde estamos exportando
 import { somar, arr } from "./ESM.js";
 const [ games ] = arr
 
 console.log(somar(4, 2))
 console.log(games)
+
+//Inportando o default, dessa vez não usamos as chaves, e ele também não precisa do "as" para ser renomeado, podemos passar o nome que quiser na importação
+import Ola from './ESM.js'
+console.log(Ola())
+
+/* Caso quisessemos inportar tudo junto, também poderiamos(Obs: Não podemos dar o nome que quisermos quando importarmos, temos que usar o mesmo nome,
+mas podemos substituir usando o "as"): */
+import { name2 as nameContador, name3 as nameW } from "./ESM.js"
+console.log(nameContador, nameW)
+
+//Se quisessemos importar o default junto também, passamos apenas o nome da default ANTES de todos:
+/* import Ola, { name2 as nameContador, name3 as nameW } from "./ESM.js" */
