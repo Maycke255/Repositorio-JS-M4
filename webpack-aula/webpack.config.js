d77a8c510219e623aaf74c4a9bee86e70076401b
Maycke <reydsonk@gmail.com>
2025-07-31 17:55:01 -0300

    Faltando estudos sobre os loaders e pluguins

--- webpack-aula/webpack.config.js
+++ webpack-aula/webpack.config.js
@@ -7,17 +7,25 @@ module.exports = {
     entry: {
         date: './src/wpack.js', /* vamos passar o nome dessa aqui, que e o arquivo do dayjs como dat, pode ser qualquer nome, o arquivo sera gerado a 
         partir dele */
-        hello: './src/hello.js' // Segundo arquivo, do hello, especificamos o caminho
+        hello: './src/hello.js', // Segundo arquivo, do hello, especificamos o caminho
+    //     style: './style/style.css' // No caso do CSS ou qualquer outro arquivo, também precisamos definir uma entrada
     }, //Você define onde o Webpack colocará o bundle final, no caso o arquivo compilado:
     output: {
         path: path.resolve(__dirname, 'dist'), /* aqui destinamos o seu caminho, como dito anteriormente __dirname refere-se ao diretorio atual onde
         estou, e dist e o nome da pasta que queremos enviar os arquivos compilados */
-        filename: '[name].min.js' /* agora uma propriedade bem importante, a filename refere-se ao nome dos arquivos, por padrão o WB da nomes aos
+        filename: '[name].min.js', /* agora uma propriedade bem importante, a filename refere-se ao nome dos arquivos, por padrão o WB da nomes aos
         arquivos de acordo com o nome inseridos na entrada, então usamos esse nome para dar nome aos arquivos, esse 'min' so quer dizer minimizado
         ele não e obrigatorio */
+        clean: true
     },
-    mode: 'production' /* E necessario quando estivermos codando, não estivermos finalizado o projeto, e necessario mudar para o modo de "development", 
+    mode: 'development', /* E necessario quando estivermos codando, não estivermos finalizado o projeto, e necessario mudar para o modo de "development", 
     o production e ultilizado apenas para finalizar tudo e deixar o codigo compilado e finalizado*/
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
+    }
 }
 
 //Se quisermos podemos ate apagar a pasta node_modules depois, que vai funcionar normalmente, se quisermos instalar de novo e so digitar
